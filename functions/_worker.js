5d871922dd734ce47a696fb2d316bbe4c9d39f4e
Alex Tan <alex.tan.toronto@hotmail.com>
2025-03-28 18:57:40 -0400

    Move worker to correct location

---
+++ functions/_worker.js
@@ -0,0 +1,12 @@
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages'
+import * as build from '../build/server/index.js'
+
+const handleRequest = createPagesFunctionHandler({
+  build,
+  mode: process.env.NODE_ENV,
+  getLoadContext: (context) => context.env,
+})
+
+export const onRequest = async (context) => {
+  return handleRequest(context)
+} 
