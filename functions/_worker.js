ed5eeca00fe4bfe9c55397c3fdd156d52eaab34b
Alex Tan <alex.tan.toronto@hotmail.com>
2025-03-28 19:27:20 -0400

    Fix worker configuration and compatibility flags

--- functions/_worker.js
+++ functions/_worker.js
@@ -9,6 +9,10 @@ const handleRequest = createPagesFunctionHandler({
 
 export default {
   async fetch(request, env, ctx) {
+    const url = new URL(request.url)
+    if (url.pathname.startsWith('/assets/')) {
+      return env.ASSETS.fetch(request)
+    }
     return handleRequest({ request, env, ctx })
   }
 } 
